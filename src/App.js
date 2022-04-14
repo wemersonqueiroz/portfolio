9a33af2e30d2b4376eb1309bffbae7d681f3bda8
Wemerson <wemersonqueiroz@yahoo.com>
2022-04-14 06:39:36 +0100

    [Feat] Imported components to App

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
+import React from "react"
+import ReactDOM from "react-dom"
+import Header from "./components/Header"
+import Home from "./components/Home"
+import Services from "./components/Services"
 import "./App.css"
 
 function App() {
-  return <div className="App"></div>
+  return (
+    <div className="App">
+      <Header />
+      <Home />
+      <Services />
+    </div>
+  )
 }
 
 export default App
