7ddf06e291e60237be593d3a8d89f299f9eb3530
Wemerson <wemersonqueiroz@yahoo.com>
2022-04-14 18:28:55 +0100

    [Feat] Importing About/Works and Footer components

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import ReactDOM from "react-dom"
 import Header from "./components/Header"
 import Home from "./components/Home"
 import Services from "./components/Services"
+import About from "./components/About"
+import Works from "./components/Works"
+import Footer from "./components/Footer"
 import "./App.css"
 
 function App() {
@@ -11,6 +14,9 @@ function App() {
       <Header />
       <Home />
       <Services />
+      <About />
+      <Works />
+      <Footer />
     </div>
   )
 }
