20fb745e496e8aa0599c6d3f64cc99897f2f9338
Wemerson <wemersonqueiroz@yahoo.com>
2022-04-14 07:05:10 +0100

    [Feat] Added About section as component

---
+++ src/components/About.js
@@ -0,0 +1,16 @@
+import React from "react"
+import profile from "../images/profile.png"
+
+export default function About() {
+  return (
+    <section className="about--me" id="about">
+      <h2 className="section__title section__title--about">About</h2>
+      <p className="section__subtitle section__subtitle--about"></p>
+      <div class="about-me__body">
+        <p></p>
+        <p></p>{" "}
+      </div>
+      <img src={profile} width="200px" alt="profile" />;
+    </section>
+  )
+}
