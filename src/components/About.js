6deec731f1a339df6cfe3026be8f7f0e172fda78
Wemerson <wemersonqueiroz@yahoo.com>
2022-04-15 17:48:06 +0100

    [Feat] Added lorem text and changed profile picture

--- src/components/About.js
+++ src/components/About.js
@@ -1,16 +1,28 @@
 import React from "react"
-import profile from "../images/profile.png"
+import profile01 from "../images/profile01.png"
 
 export default function About() {
   return (
-    <section className="about--me" id="about">
-      <h2 className="section__title section__title--about">About</h2>
-      <p className="section__subtitle section__subtitle--about"></p>
+    <section className="about-me" id="about">
+      <h2 className="section__title section__title--about">Who I am</h2>
+      <p className="section__subtitle section__subtitle--about">
+        Self thought Front end Developer
+      </p>
       <div class="about-me__body">
-        <p></p>
-        <p></p>{" "}
+        <p>
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat.
+        </p>
+        <p>
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat.
+        </p>
       </div>
-      <img src={profile} width="200px" alt="profile" />
+      <img src={profile01} alt="profile" className="about-me__img" />
     </section>
   )
 }
