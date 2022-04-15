544c3ef7252f10af1863ac70416430f184a66cc9
Wemerson <wemersonqueiroz@yahoo.com>
2022-04-15 17:50:31 +0100

    [Feat] Added background image at the section and lorem text

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,21 +1,41 @@
 import React from "react"
+import services from "../images/services.jpg"
 
 export default function Services() {
   return (
-    <section className="my--services" id="my-services">
+    <section
+      style={{ backgroundImage: `url(${services})` }}
+      className="my-services"
+      id="my-services">
       <h2 className="section__title section__title--services"> What I do </h2>
       <div className="services">
         <div className="service">
-          <h3>h3</h3>
-          <p>p</p>
+          <h3>Web Development</h3>
+          <p>
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat.
+          </p>
         </div>
         <div className="service">
           <h3>Web Development</h3>
-          <p> lorem ipsum dolor sit amet, consectetur</p>
+          <p>
+            {" "}
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat.
+          </p>
         </div>
         <div className="service">
           <h3>Web Development</h3>
-          <p>lorem ipsum dolor sit amet, consectetur</p>
+          <p>
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat.
+          </p>
         </div>
       </div>
       <a href="#work" className="btn">
