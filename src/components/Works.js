cd778182c6f4263f4333ba7f05f5daeb0fcb0023
Wemerson <wemersonqueiroz@yahoo.com>
2022-04-15 17:46:58 +0100

    [Feat] Added 6 demo images as portfolio and imported 2 images

--- src/components/Works.js
+++ src/components/Works.js
@@ -1,14 +1,36 @@
 import React from "react"
-import profile from "../images/profile.png"
+import portfolio00 from "../images/portfolio00.jpg"
+import portfolio01 from "../images/portfolio01.jpg"
 
 export default function Works() {
   return (
     <section className="my-work" id="works">
-      <h2 class="section__title"> My Work</h2>
-      <p class="section__subtitle"></p>
-      <div class="portifolio">
-        <a href="#" class="portifolio__item" alt="">
-          <img src={profile} alt="" className="portifolio__img" />
+      <h2 class="section__title section__title--work"> My Work</h2>
+      <p class="section__subtitle">A selection of my range of work</p>
+      <div class="portfolio">
+        {/* portfolio ITEM 01*/}
+        <a href="#" class="portfolio__item" alt="">
+          <img src={portfolio00} alt="" className="portfolio__img" />
+        </a>
+        {/* portfolio ITEM 02*/}
+        <a href="#" class="portfolio__item" alt="">
+          <img src={portfolio01} alt="" className="portfolio__img" />
+        </a>
+        {/* portfolio ITEM 03*/}
+        <a href="#" class="portfolio__item" alt="">
+          <img src={portfolio00} alt="" className="portfolio__img" />
+        </a>
+        {/* portfolio ITEM 04*/}
+        <a href="#" class="portfolio__item" alt="">
+          <img src={portfolio01} alt="" className="portfolio__img" />
+        </a>
+        {/* portfolio ITEM 05*/}
+        <a href="#" class="portfolio__item" alt="">
+          <img src={portfolio00} alt="" className="portfolio__img" />
+        </a>
+        {/* portfolio ITEM 06*/}
+        <a href="#" class="portfolio__item" alt="">
+          <img src={portfolio01} alt="" className="portfolio__img" />
         </a>
       </div>
     </section>
