766e72e229d885fda6eb5798b0483eaf3867c4d2
Wemerson <wemersonqueiroz@yahoo.com>
2022-04-15 17:59:52 +0100

    [Feat] Added function to open hamburger button when clicked

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,35 +1,43 @@
 import React from "react"
 
 export default function Nav() {
+  function closeNav() {
+    document.body.classList.remove("nav-open")
+  }
   return (
-    <>
-      <button className="nav--toggle" aria-label="toggle navigation">
-        <span className="hamburguer"> </span>{" "}
-      </button>
-      <nav className="nav">
-        <ul className="nav__list">
-          <li class="nav__item">
-            {" "}
-            <a href="#">Home</a>
-          </li>
-          <li class="nav__item">
-            {" "}
-            <a href="#">My Services</a>
-          </li>
-          <li class="nav__item">
-            {" "}
-            <a href="#">About me</a>
-          </li>
-          <li class="nav__item">
-            {" "}
-            <a href="#">My Work</a>
-          </li>
-          <li class="nav__item">
-            {" "}
-            <a href="#">Contact</a>
-          </li>
-        </ul>
-      </nav>
-    </>
+    <nav onClick={closeNav} className="nav">
+      <ul className="nav__list">
+        <li className="nav__item">
+          {" "}
+          <a href="#" className="nav__link">
+            Home
+          </a>
+        </li>
+        <li class="nav__item">
+          {" "}
+          <a href="#" className="nav__link">
+            My Services
+          </a>
+        </li>
+        <li class="nav__item">
+          {" "}
+          <a href="#" className="nav__link">
+            About me
+          </a>
+        </li>
+        <li class="nav__item">
+          {" "}
+          <a href="#" className="nav__link">
+            My Work
+          </a>
+        </li>
+        <li class="nav__item">
+          {" "}
+          <a href="#" className="nav__link">
+            Contact
+          </a>
+        </li>
+      </ul>
+    </nav>
   )
 }
