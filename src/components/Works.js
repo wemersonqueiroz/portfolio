1453342b5e6050231d66b0160a32e58d1e6be7d0
Wemerson <wemersonqueiroz@yahoo.com>
2022-05-03 18:32:57 +0100

    [UPDATE]Updated param documentation

--- src/components/Works.js
+++ src/components/Works.js
@@ -1,5 +1,10 @@
 import React from "react"
 
+/**
+ * @param {string} projectLink e.g "www.wemerson.dev"/ "githubPage"
+ * @param {string} projectImage e.g {Images.category.filename}
+ */
+
 const WorksDetails = props => {
   const { projectLink, projectImage } = props
   return (
