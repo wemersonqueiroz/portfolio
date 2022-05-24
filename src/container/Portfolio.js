f9241a969ce8135e5c33b08de3a4d50022420244
Wemerson <wemersonqueiroz@yahoo.com>
2022-05-24 07:04:51 +0100

    [UPDATE]Arranged portfolio order with latest project

--- src/container/Portfolio.js
+++ src/container/Portfolio.js
@@ -8,21 +8,20 @@ export default function Works() {
       <h2 className="section__title section__title--work"> My Work</h2>
       <p className="section__subtitle">A selection of my range of work</p>
       <div class="portfolio">
-        {/* portfolio ITEM 00*/}
+        {/* portfolio ITEM 03*/}
         <WorksDetails
-          projectLink="www.wemerson.dev"
-          projectImage={Images.portfolio.portfolio00}
+          projectLink="https://manage-tailwind.pages.dev/"
+          projectImage={Images.portfolio.portfolio03}
+        />
+        {/* portfolio ITEM 02*/}
+        <WorksDetails
+          projectLink="https://wemersonqueiroz.github.io/bootstrap-tindog/"
+          projectImage={Images.portfolio.portfolio02}
         />
         {/* portfolio ITEM 01*/}
         <WorksDetails
           projectLink="https://hidden-ravine-54566.herokuapp.com/"
           projectImage={Images.portfolio.portfolio01}
-        />
-
-        {/* portfolio ITEM 02*/}
-        <WorksDetails
-          projectLink="https://wemersonqueiroz.github.io/bootstrap-tindog/"
-          projectImage={Images.portfolio.portfolio02}
         />
       </div>
     </section>
